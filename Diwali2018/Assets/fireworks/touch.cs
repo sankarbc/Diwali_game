59cdf6da6cfe4f4e28908a1470c90d784315bd91
sankarbc <sankar@nithra.mobi>
2018-09-24 11:40:47 +0530

    hun
    defs

--- Diwali2018/Assets/fireworks/touch.cs
+++ Diwali2018/Assets/fireworks/touch.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class touch : MonoBehaviour {
     public Canvas show_up;
     public Canvas show_down;
+    public GameObject nilaa;
+   
+   
+ 
     void Start () {
         show_up.enabled = true;
         show_down.enabled = false;
+        nilaa.SetActive(false);
+
+
     }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void show()
+    {
+       
+
+       // string s = gameObject.transform.name;
+        if (nilaa.name=="nila")
+        {
+           
+            show_down.enabled = true;
+            nilaa.SetActive(true);
+           
+           
+        }
+        
+    }
 }
