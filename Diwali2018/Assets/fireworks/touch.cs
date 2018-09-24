dc507e344d76ca72788bfa0138c1bd06104d5177
sankarbc <sankar@nithra.mobi>
2018-09-24 10:57:45 +0530

    full
    Work

---
+++ Diwali2018/Assets/fireworks/touch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour {
+    public Canvas show_up;
+    public Canvas show_down;
+    void Start () {
+        show_up.enabled = true;
+        show_down.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
